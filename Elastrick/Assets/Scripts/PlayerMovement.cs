28a5b7780d00fda41b8f4d987f5bc956902cd337
Jacob Lee <jslee@mail.bradley.edu>
2023-04-09 23:14:31 -0500

    Revert "Player Movement Down (Alpha Rendition)"
    This reverts commit 4ba526bda7cc0086743865339b64dda017bada9f.

--- Elastrick/Assets/Scripts/PlayerMovement.cs
+++ Elastrick/Assets/Scripts/PlayerMovement.cs
@@ -1,98 +1,82 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
-public class PlayerMovement: MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
-    Vector2 rotation;
-    PlayerActions pActions;
+    public static PlayerMovement main;
 
-    [Header("Linkage")]
-    [SerializeField] private GameObject rotObject;
-    [SerializeField] private Rigidbody2D rb2D;
+    private Camera mainCam;
+    public Vector3 mousePos;
+    public Transform aimTransform;
+    public bool canFire = true;
+    private float timer;
+    public float fireCooldown;
 
-    [Header("Launch Check")]
-    [SerializeField] private bool canLaunch = true;
+    public bool hasCollided = true;
 
-    [Header("Speed")]
-    [SerializeField] private float launchVelocity = 300f;
 
-    // For stopping on collision.
-    private bool check;
 
     // Start is called before the first frame update
-    void Awake()
+    void Start()
     {
-        // Links pActions to PlayerActions
-        pActions = new PlayerActions();
-
-        // Binds the Rotation of controller to rotation (vector2)
-        pActions.PlayerActionMap.Rotate.performed += ctx => rotation =
-        ctx.ReadValue<Vector2>();
-
-        // Binds the Launch from player's action map to Launch();.
-        pActions.PlayerActionMap.Launch.performed += ctx => Launch();
+        mainCam = 
+        GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
     }
 
-    // Every frame, check if the player has rotated at all.
+    private void Awake()
+    {
+        if (main == null)
+        {
+            main = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+
+    // Update is called once per frame
     void Update()
     {
-        RotatePlayer();
-    }
-    
-    // On enable.
-    private void OnEnable()
-    {
-        pActions.PlayerActionMap.Enable();
-    }
-    
-    // On disable.
-    private void OnDisable()
-    {
-        pActions.PlayerActionMap.Disable();
-    }
+        mousePos = mainCam.ScreenToWorldPoint(Input.mousePosition);
 
-    // Rotates the player.
-    private void RotatePlayer()
-    {
-        float angle = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-    }
+        Vector3 rotation = mousePos - transform.position;
 
-    // If the player launches, they cannot shoot immediately again. Also
-    // adds a force where the player is aiming.
-    private void Launch()
-    {
-        if (canLaunch)
+        float rotZ = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;
+
+        transform.rotation = Quaternion.Euler(0, 0, rotZ);
+
+        if (!canFire)
         {
-            canLaunch = false;
-            Debug.Log("Boing!");
-            rb2D.AddForce(rotObject.transform.right * launchVelocity);
+            timer += Time.deltaTime;
+            if(timer > fireCooldown)
+            {
+                canFire = true;
+                timer = 0;  
+            }
         }
 
-
+        if (Input.GetMouseButton(0) && canFire && hasCollided)
+        {
+            canFire = false;
+            hasCollided = false;
+            ShootScript.main.Shoot();
+            Debug.Log(ShootScript.main.force);
+        }
     }
 
-    // Stop the player after collision.
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        // If you cannot launch, stop your movement as you've likely hit a
-        // wall or something.
-        if(!canLaunch && check == false)
-        {
-            StartCoroutine(StopMovement());
-        }
+        StopAllCoroutines();
+        GetComponent<Rigidbody2D>().velocity = new Vector2(0f, 0f);
+        StartCoroutine(TimeAfterCollision());
     }
 
-    // Stops movement, then after 0.1 seconds (to allow graphic to catch up to
-    // unity's behind the scenes mathematics, allows the user to relaunch.
-    IEnumerator StopMovement()
+    IEnumerator TimeAfterCollision()
     {
-        check = true;
-        rb2D.velocity = Vector2.zero;
-        yield return new WaitForSeconds(0.1f);
-        canLaunch = true;
-        check = false;
+        yield return new WaitForSeconds(3);
+        hasCollided = true;
+        canFire = true;
     }
 }
