f963358df7403bb79d60cd40278e6bb26ccbc166
InvsbleBeat87 <117841703+InvsbleBeat87@users.noreply.github.com>
2023-04-13 15:33:47 -0500

    platform work

---
+++ Elastrick/Assets/Scripts/PlatformBehavior.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformBehavior : MonoBehaviour
+{
+    public float TopY = 10f;
+    public float BotY = 0.0f;
+
+    public float speed = 2f;
+
+    public Vector3 movement;
+
+    private bool goingDown = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.y < TopY && goingDown == false)
+        {
+            movement = Vector3.up * speed * Time.deltaTime;
+            transform.Translate(movement);
+        }
+        else if(transform.position.y >= TopY)
+        {
+            goingDown = true;
+        }
+
+        if(transform.position.y > BotY && goingDown == true)
+        {
+            movement = -(Vector3.up * speed * Time.deltaTime);
+            transform.Translate(movement);
+        }
+        else if(transform.position.y <= BotY)
+        {
+            goingDown = false;
+        }
+    }
+}
