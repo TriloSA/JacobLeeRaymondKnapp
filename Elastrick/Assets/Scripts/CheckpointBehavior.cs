acd97f5023a8837e44b9f2a6eca1f3d6baac089f
Jacob Lee <jslee@mail.bradley.edu>
2023-05-01 19:33:36 -0500

    Checkpoint 2Player Fix
    nuff said

--- Elastrick/Assets/Scripts/CheckpointBehavior.cs
+++ Elastrick/Assets/Scripts/CheckpointBehavior.cs
@@ -14,6 +14,8 @@ public class CheckpointBehavior : MonoBehaviour
 {
     public AudioClip checkpoint;
 
+    public bool hasUnlockedP1;
+    public bool hasUnlockedP2;
 
     /// <summary>
     /// On Trigger, set the new spawn coordinate values in relation to the 
@@ -26,28 +28,35 @@ public class CheckpointBehavior : MonoBehaviour
         // Safety measure.
         if (collision.gameObject.CompareTag("Player"))
         {
-            Debug.Log("Checkpoint Unlocked!");
-
-            AudioManager.inst.PlaySound(checkpoint);
-            StartCoroutine(FlickerFlag());
-
-            // Checkpoint is now no longer collidable so you can't "unlock"
-            // and older checkpoint. So keep moving, slacker! NOTE: This will
-            // CURRENTLY NOT work in 2 player since it will disable for the
-            // other player.
-            this.gameObject.GetComponent<Collider2D>().enabled = false;
             // newSpawn's values is now the same as the checkpoint's.
             Vector3 newSpawn = this.gameObject.transform.position;
             
             // If they are player 1, spawn them on the left of the check point.
             // Otherwise, spawn them on the right.
-            if (collision.gameObject.GetComponent<PlayerBehavior>().isPlayer1)
+
+            if (collision.gameObject.GetComponent<PlayerBehavior>().isPlayer1 && !hasUnlockedP1)
             {
+                Debug.Log("Checkpoint Unlocked!");
+
                 newSpawn = new Vector3(newSpawn.x - 1, newSpawn.y, newSpawn.z);
+                // Checkpoint is now no longer collidable so you can't "unlock"
+                // and older checkpoint. So keep moving, slacker!
+                hasUnlockedP1 = true;
+
+                AudioManager.inst.PlaySound(checkpoint);
+                StartCoroutine(FlickerFlag());
             }
-            else
+            else if (!collision.gameObject.GetComponent<PlayerBehavior>().isPlayer1 && !hasUnlockedP2)
             {
+                Debug.Log("Checkpoint Unlocked!");
+
                 newSpawn = new Vector3(newSpawn.x + 1, newSpawn.y, newSpawn.z);
+                // Checkpoint is now no longer collidable so you can't "unlock"
+                // and older checkpoint. So keep moving, slacker!
+                hasUnlockedP2 = true;
+
+                AudioManager.inst.PlaySound(checkpoint);
+                StartCoroutine(FlickerFlag());
             }
 
             // Value X
